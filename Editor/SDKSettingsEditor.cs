6b780e687a34ceaf11e14e4aee11753d89b8e4ff
Be4Die <misha375375@gmail.com>
2025-06-13 02:13:29 +0400

    fix missing background.jpg

--- Editor/SDKSettingsEditor.cs
+++ Editor/SDKSettingsEditor.cs
@@ -9,7 +9,7 @@ namespace WelwiseGames.Editor
     public class SDKSettingsEditor : EditorWindow
     {
         // ReSharper disable once InconsistentNaming
-        public const string PACKAGE_VERSION = "0.0.6";
+        public const string PACKAGE_VERSION = "0.0.7";
         
         private SDKSettings _settings;
         private SerializedObject _serializedSettings;
