932a017ec87587575965d64317428eac15493ce3
Be4Die <misha375375@gmail.com>
2025-06-22 19:14:11 +0400

    update template

--- Editor/SDKSettingsEditor.cs
+++ Editor/SDKSettingsEditor.cs
@@ -10,7 +10,7 @@ namespace WelwiseGames.Editor
     public class SDKSettingsEditor : EditorWindow
     {
         // ReSharper disable once InconsistentNaming
-        public const string PACKAGE_VERSION = "0.0.8";
+        public const string PACKAGE_VERSION = "0.0.9";
         
         private SDKSettings _settings;
         private SerializedObject _serializedSettings;
