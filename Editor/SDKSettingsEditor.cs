556cbacdee6e2a15e64bdf9bfe28d7af5b85cda8
Be4Die <misha375375@gmail.com>
2025-08-09 17:41:21 +0400

    fix planet prerenderer css for yandex browser

--- Editor/SDKSettingsEditor.cs
+++ Editor/SDKSettingsEditor.cs
@@ -13,7 +13,7 @@ namespace WelwiseGames.Editor
 {
     public class SDKSettingsEditor : EditorWindow
     {
-        public const string TemplateVersion = "0.0.15";
+        public const string TemplateVersion = "0.0.16";
         private const string LogoFileName = "__ws-logo";
         
         private enum TabType
